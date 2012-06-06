024d08f5acd28d532e9df9393b797fcb0928a45d
Loic Sharma <sharma.loic@gmail.com>
2012-06-06 01:54:42 -0500

    Removed the test route

--- routes.php
+++ routes.php
@@ -11,15 +11,3 @@ Route::any('directus/table/(:any)/edit/(:num)', 'directus::tables@edit');
 Route::any('directus/login', 'directus::user@login');
 Route::any('directus/forgot', 'directus::user@forgot');
 Route::any('directus/logout', 'directus::user@logout');
-
-Route::any('directus/test', function ()
-{
-	$user = new Directus\User;
-
-	$user->email      = 'sharma.loic@gmail.com';
-	$user->password   = 'corinne';
-	$user->first_name = 'Loic';
-	$user->last_name  = 'Sharma';
-
-	$user->save();
-});
